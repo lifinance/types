7adf21baa809c388421b10dad51d18731829372e
Leonardo Cascianelli <leo@li.finance>
2023-02-13 11:15:54 +0100

    feat: added flag for variable price impact (#127)

--- src/api.ts
+++ src/api.ts
@@ -38,6 +38,7 @@ export interface RouteOptions {
   exchanges?: AllowDenyPrefer
   fee?: number // 0.03 = take 3% integrator fee (requires verified integrator to be set)
   insurance?: boolean // whether the user want to insure their tx
+  hideRoutesWithGTEPriceImpact?: number // hide routes with price impact greater than or equal to this value
 }
 
 export type ToolsResponse = {
@@ -142,6 +143,7 @@ export interface QuoteRequest extends ToolConfiguration {
   fee?: number | string
   insurance?: boolean // indicates whether the user wants a quote with bridge insurance
   allowDestinationCall?: boolean // (default : true) // destination calls are enabled by default
+  hideRoutesWithGTEPriceImpact?: number // hide routes with price impact greater than or equal to this value
 }
 
 export interface ContractCallQuoteRequest extends ToolConfiguration {
@@ -164,6 +166,7 @@ export interface ContractCallQuoteRequest extends ToolConfiguration {
   referrer?: string
   fee?: number | string
   allowDestinationCall?: boolean // (default : true) // destination calls are enabled by default
+  hideRoutesWithGTEPriceImpact?: number // hide routes with price impact greater than or equal to this value
 }
 
 export interface ContractCallQuotesRequest extends ToolConfiguration {
