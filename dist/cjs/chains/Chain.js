cbf142a6bf44ac0f344d402ddbc18e472d443b25
h3xept <leo@li.finance>
2023-03-22 11:41:36 +0100

    chore: removed dist for merge

--- dist/cjs/chains/Chain.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ChainType = void 0;
-var ChainType;
-(function (ChainType) {
-    ChainType["EVM"] = "EVM";
-    ChainType["Solana"] = "SOLANA";
-})(ChainType = exports.ChainType || (exports.ChainType = {}));
