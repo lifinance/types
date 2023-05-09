ccf5bd1cd069097db70b021a3f964f4a81411dcb
Leonardo Cascianelli <leo@li.finance>
2023-05-09 17:37:42 +0200

    chore: removed dist

--- dist/chains/Chain.js
+++
@@ -1,5 +0,0 @@
-export var ChainType;
-(function (ChainType) {
-    ChainType["EVM"] = "EVM";
-    ChainType["Solana"] = "SOLANA";
-})(ChainType || (ChainType = {}));
--- dist/chains/supported.chains.d.ts
+++
@@ -1,5 +0,0 @@
-import { EVMChain } from './EVMChain';
-import { SolanaChain } from './SolanaChain';
-export declare const supportedEVMChains: EVMChain[];
-export declare const supportedSolanaChains: SolanaChain[];
-export declare const supportedChains: EVMChain[];
