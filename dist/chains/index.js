30283cfe6d3b7d431a1b2a14c213b47c9a660e37
Daniela Chybisova <iron.melmel@gmail.com>
2023-06-06 13:37:29 +0200

    chore: removed dist

--- dist/chains/index.js
+++
@@ -1,5 +0,0 @@
-export * from './Chain';
-export * from './chain.utils';
-export * from './EVMChain';
-export * from './SolanaChain';
-export * from './supported.chains';
--- dist/cjs/apiErrors.d.ts
+++
@@ -1,9 +0,0 @@
-import { Action } from './step';
-export type ToolErrorType = 'NO_QUOTE';
-export interface ToolError {
-    errorType: ToolErrorType;
-    code: string;
-    action: Action;
-    tool: string;
-    message: string;
-}
