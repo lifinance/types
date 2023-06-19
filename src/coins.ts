3137df761373fd642dfe03ae4e4fb7c6c21c3741
Leonardo Cascianelli <leo@li.finance>
2023-06-19 16:30:11 +0200

    chore: fixed usdc.e symbol

--- src/coins.ts
+++ src/coins.ts
@@ -928,7 +928,7 @@ const basicCoins: BasicCoin[] = [
     key: CoinKey.USDCe,
     name: CoinKey.USDCe,
     logoURI:
-      'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664/logo.png',
+      'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
     verified: true,
     chains: {
       [ChainId.ARB]: {
