061d4b4a9babef035c43d3ce211da8c2f18ffa68
Leonardo Cascianelli <leo@li.finance>
2023-06-19 16:19:28 +0200

    chore: renamed usdcoin

--- src/coins.ts
+++ src/coins.ts
@@ -804,7 +804,7 @@ const basicCoins: BasicCoin[] = [
   // USDC
   {
     key: CoinKey.USDC,
-    name: CoinKey.USDC,
+    name: 'USD Coin',
     logoURI:
       'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
     verified: true,
