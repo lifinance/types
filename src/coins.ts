67d39bb60423f96f142d38e3ff2e6466d2f8f0a8
Max Klenk <max@klenk.biz>
2023-07-01 07:35:03 +0200

    fix: set right polygon zkevm WETH address

--- src/coins.ts
+++ src/coins.ts
@@ -1653,7 +1653,7 @@ export const wrappedTokens: { [ChainId: string]: StaticToken } = {
   },
   [ChainId.PZE]: {
     // https://zkevm.polygonscan.com/token/0x4f9a0e7fd2bf6067db6994cf12e4495df938e6e9
-    address: '0x5aea5775959fbc2557cc8789bc1bf90a239d9a91',
+    address: '0x4f9a0e7fd2bf6067db6994cf12e4495df938e6e9',
     symbol: 'WETH',
     decimals: 18,
     chainId: ChainId.PZE,
