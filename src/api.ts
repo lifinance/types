fcd84649b22cb68ea2c8b04927f320e9c164c66f
kristohberg <kristoffer.hovland.berg@gmail.com>
2023-09-13 09:31:34 +0200

    feat: add chainRequest for the /chains endpoint

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,7 @@ import {
   BridgeDefinition,
   Chain,
   ChainId,
+  ChainType,
   ExchangeDefinition,
   LifiStep,
   Token,
@@ -240,6 +241,7 @@ export interface ConnectionsRequest extends ToolConfiguration {
   fromToken?: string
   toChain?: number | string
   toToken?: string
+  chainType?: ChainType
 }
 
 export interface Connection {
@@ -369,12 +371,17 @@ export interface ChainsResponse {
   chains: ExtendedChain[]
 }
 
+export interface ChainsRequest {
+  chainType?: ChainType
+}
+
 export interface ToolsRequest {
   chains?: ChainId[]
 }
 
 export type TokensRequest = {
   chains?: ChainId[]
+  chainType?: ChainType
 }
 
 export type TokensResponse = {
