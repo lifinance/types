10b5f207b7279bebc656c09da73dd539a9ea9763
Eugene Chybisov <imchybisov@gmail.com>
2023-10-05 16:23:39 +0200

    fix: ci and merge conflicts

---
+++ src/errors.ts
@@ -0,0 +1,15 @@
+// Error codes emitted by the LI.FI API
+export enum ErrorCode {
+  DefaultError = 1000,
+  FailedToBuildTransactionError = 1001,
+  NoQuoteError = 1002,
+  NotFoundError = 1003,
+  NotProcessableError = 1004,
+  RateLimitError = 1005,
+  ServerError = 1006,
+  SlippageError = 1007,
+  ThirdPartyError = 1008,
+  TimeoutError = 1009,
+  UnauthorizedError = 1010,
+  ValidationError = 1011,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './api.js'
 export * from './bridges.js'
 export * from './chains/index.js'
+export * from './errors.js'
 export * from './exchanges.js'
 export * from './quests.js'
 export * from './step.js'
