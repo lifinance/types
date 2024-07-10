ba2373bfa05b1a0b0a0df44e01e23beb81d646cf
Eugene Chybisov <18644653+chybisov@users.noreply.github.com>
2024-07-10 11:03:17 +0200

    feat: remove insurance (#246)
    Co-authored-by: myz1237 <mengyizhuoprivate@outlook.com>

--- src/api.ts
+++ src/api.ts
@@ -77,7 +77,6 @@ export interface RoutesRequest {
 export interface RouteOptions {
   integrator?: string // Should contain the identifier of the integrator. Usually, it's dApp/company name.
   fee?: number // 0.03 = take 3% integrator fee (requires verified integrator to be set)
-  insurance?: boolean // Whether the user wants to insure their tx
   maxPriceImpact?: number // Hide routes with price impact greater than or equal to this value
   order?: Order // (default: CHEAPEST) 'FASTEST' | 'CHEAPEST'
   slippage?: number // (default: 0.03) Expressed as decimal proportion, 0.03 represents 3%
@@ -86,6 +85,11 @@ export interface RouteOptions {
   allowDestinationCall?: boolean // (default: true) destination calls are enabled by default
   bridges?: AllowDenyPrefer
   exchanges?: AllowDenyPrefer
+
+  /**
+   * @deprecated This property is deprecated and will be removed in future versions.
+   */
+  insurance?: boolean // Whether the user wants to insure their tx
 }
 
 export type ToolsResponse = {
@@ -112,13 +116,22 @@ export interface AllowDenyPrefer {
   prefer?: string[] // (default: []) // eg. ['1inch'] to use 1inch if available and fall back to others if not
 }
 
+/**
+ * @deprecated _InsuranceState is deprecated and will be removed in future versions.
+ */
 export const _InsuranceState = [
   'INSURED',
   'INSURABLE',
   'NOT_INSURABLE',
 ] as const
+/**
+ * @deprecated InsuranceState is deprecated and will be removed in future versions.
+ */
 export type InsuranceState = (typeof _InsuranceState)[number]
 
+/**
+ * @deprecated InsuranceState is deprecated and will be removed in future versions.
+ */
 export interface Insurance {
   state: InsuranceState
   feeAmountUsd: string
@@ -233,10 +246,14 @@ export interface QuoteRequest extends ToolConfiguration {
   integrator?: string
   referrer?: string
   fee?: number | string
-  insurance?: boolean // indicates whether the user wants a quote with bridge insurance
   allowDestinationCall?: boolean // (default : true) // destination calls are enabled by default
   fromAmountForGas?: string // the amount of token to convert to gas
   maxPriceImpact?: number // hide routes with price impact greater than or equal to this value
+
+  /**
+   * @deprecated This property is deprecated and will be removed in future versions.
+   */
+  insurance?: boolean // indicates whether the user wants a quote with bridge insurance
 }
 
 export interface ContractCall {
