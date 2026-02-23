f5aba6687884a3f0a39b7fb5c1bf4db3a6d2fb4d
Nathan DM <nathan.demaestri@gmail.com>
2026-02-23 10:22:24 +0100

    chore: create new SVMChainId

--- src/chains/SVMChain.ts
+++ src/chains/SVMChain.ts
@@ -1,5 +1,8 @@
 import type { _Chain } from './Chain.js'
+import type { ChainId } from './base.js'
 import type { AddEthereumChainParameter } from './EVMChain.js'
+
+export type SVMChainId = ChainId.SOL | ChainId.FOG
 
 export interface SVMChain extends _Chain {
   tokenlistUrl?: string
