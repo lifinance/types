28ba41e236714c86eac258d57b2eb1ad331495eb
myz1237 <mengyizhuoprivate@outlook.com>
2026-03-13 23:34:14 +0800

    feat: add frxUSD token and bitget logo

--- src/tokens/base.ts
+++ src/tokens/base.ts
@@ -95,6 +95,7 @@ export enum CoinKey {
   USDm = 'USDm', // Stablecoin on MegaETH: MegaUSD
   USD1 = 'USD1', // World Liberty Financial USD
   PathUSD = 'PathUSD', // Stablecoin on Tempo chain
+  frxUSD = 'frxUSD', // Stablecoin on Fraxtal chain
 
   // Other tokens
   WBTC = 'WBTC',
