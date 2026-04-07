6225771ef9c5088784c819a9da2cbdbe51286da8
Daniela Chybisova <iron.melmel@gmail.com>
2026-04-07 12:17:31 +0200

    feat: add optional txId to status request (#482)

--- src/api.ts
+++ src/api.ts
@@ -512,6 +512,7 @@ export type GetStatusRequest = {
   bridge?: string
   fromChain?: number | string
   toChain?: number | string
+  transactionId?: string
 } & ({ txHash: string } | { taskId: string })
 
 export interface BaseTransactionInfo {
