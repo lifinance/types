d8ea4f04733dece23c41fbc87bfd81b78d406064
Ndiaye Abdoul <abdoul@li.finance>
2026-09-15 10:03:38 +0100

    feat: add optional referrer to TransferMetadata (#566)

--- src/api.ts
+++ src/api.ts
@@ -796,6 +796,7 @@ export interface FailedStatusData extends BaseStatusData {
 export type StatusResponse = FullStatusData | StatusData | FailedStatusData
 interface TransferMetadata {
   integrator: string
+  referrer?: string
 }
 
 export type IncludedStep = {
